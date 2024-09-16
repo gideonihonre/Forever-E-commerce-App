b031322683db03dc3ba5d6ebb010acaff26ae2ac
gideonihonre <gideonihonre345@gmail.com>
2024-09-16 22:46:40 +0100

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import App from "./App.jsx"
+import { BrowserRouter } from "react-router-dom"
+import "./index.css"
+import ShopContextProvider, { ShopContext } from "./context/ShopContext.jsx"
+
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <ShopContextProvider>
+      <App />
+    </ShopContextProvider>
+  </BrowserRouter>
+)
