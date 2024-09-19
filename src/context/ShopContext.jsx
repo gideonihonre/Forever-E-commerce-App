08e0b92f262f7ed8dd30f893d971561700244a5e
gideonihonre <gideonihonre345@gmail.com>
2024-09-19 16:59:28 +0100

    changes made to searchbar, navbar and context

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -7,11 +7,16 @@ const ShopContextProvider = (props) => {
   const currency = "$"
   const delivery_fee = 10
   const [search, setSearch] = useState("")
+  const [showSearch, setShowSearch] = useState(false)
 
   const value = {
     products,
     currency,
-    delivery_fee
+    delivery_fee,
+    search,
+    setSearch,
+    showSearch,
+    setShowSearch
   }
 
   return (
